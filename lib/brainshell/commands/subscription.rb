681a38af96715327c35762952ccef8ea1d66dcaf
koss <koss.lebedev@gmail.com>
2016-07-08 21:08:03 +0500

    Allow to pass extra-columns to subscription sub-command

--- lib/brainshell/commands/subscription.rb
+++ lib/brainshell/commands/subscription.rb
@@ -1,28 +1,26 @@
 module Commands
   class Subscription < Thor
 
-    desc 'find ID', 'Returns subscription details by identifier'
-    def find(id)
-      data = [
-          ['row1col1', 'row1col2'],
-          ['row2col1', 'row2col2']
-      ]
+    class_option :include, type: :array, default: []
 
-      Thor::Shell::Basic.new.print_table(data)
+    desc 'find ID', 'Returns subscription by identifier'
+    def find(id)
+      subscription = Braintree::Subscription.find(id)
+
+      table = subscriptions_table([subscription])
+      shell.print_table(table)
     end
 
     subscription_statuses = Braintree::Subscription::Status::All
 
-    desc "in_status [#{subscription_statuses.join(', ')}]", 'Get subscription IDS that are in specified status'
+    desc "in_status [#{subscription_statuses.join(', ')}]", 'Get subscriptions in specified status'
     def in_status(status)
       search_results = Braintree::Subscription.search do |search|
         search.status.is status
       end
 
-      subscriptions = search_results.to_a
-
-      table = subscriptions_table(subscriptions)
-      Thor::Shell::Basic.new.print_table(table)
+      table = subscriptions_table(search_results.to_a)
+      shell.print_table(table)
     end
 
   private
@@ -42,7 +40,11 @@ module Commands
           subscription.created_at,
           subscription.plan_id,
           subscription.status
-      ]
+      ] + includes(subscription)
+    end
+
+    def includes(subscription)
+      options[:include].map { |include| subscription.send(include) }
     end
 
   end
